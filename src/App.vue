99d6b31527ad3570f4e196471af156f7dbf0b887
Nena Streeter <nenastreeter@gmail.com>
2020-08-07 13:52:13 -0500

    Finished Section 12, Directives

--- src/App.vue
+++ src/App.vue
@@ -2,144 +2,17 @@
 
 <template>
     <div class="childscalmsdowns">
-        <form>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <h1>File a Complaint</h1>
-                    <hr>
-                    <div class="form-group">
-                        <label for="email">Mail</label>
-                        <input
-                                type="text"
-                                id="email"
-                                class="form-control"
-                                v-model="userData.email"
-                                >
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input
-                                type="password"
-                                id="password"
-                                class="form-control"
-                                v-model.lazy="userData.password"
-                                >
-                                <p>{{ userData.password }}</p>
-                    </div>
-                    <div class="form-group">
-                        <label for="age">Age</label>
-                        <input
-                                type="number"
-                                id="age"
-                                class="form-control"
-                                v-model="userData.age"
-                                >
-                    </div>
-
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-                    <label for="message">Message</label><br>
-                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
-                    <textarea
-                            id="message"
-                            rows="5"
-                            class="form-control"
-                            v-model=" message "></textarea>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <div class="form-group">
-                        <label for="sendmail">
-                            <input
-                                    type="checkbox"
-                                    id="sendmail"
-                                    value="SendMail"
-                                    v-model="sendMail"
-                                    > Send Mail
-                        </label>
-                        <label for="sendInfomail">
-                            <input
-                                    type="checkbox"
-                                    id="sendInfomail"
-                                    value="SendInfoMail"
-                                    v-model="sendMail"
-                                    > Send Infomail
-                        </label>
-                    </div>
-
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-                    <label for="male">
-                        <input
-                                type="radio"
-                                id="male"
-                                value="Male"
-                                v-model="gender"
-                                > Male
-                    </label>
-                    <label for="female">
-                        <input
-                                type="radio"
-                                id="female"
-                                value="Female"
-                                v-model="gender"
-                                > Female
-                    </label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
-                    <label for="priority">Priority</label>
-                    <select
-                            id="priority"
-                            class="form-control"
-                            v-model="selectedPriority"
-                            >
-                        <option 
-                        v-for="priority in priorities" v-bind:key="priority" :selected="priority == Medium">
-                        {{ priority }}</option>
-                    </select>
-                </div>
-            </div>
-            <hr>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <app-switch v-model="dataSwitch"></app-switch>
-                    <button
-                            class="btn btn-primary"
-                            @click.prevent="submitted">Submit!
-                    </button>
-                </div>
-            </div>
-        </form>
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6  col-md=offset-3">
+        <h1>Built in Directives</h1>
+        <p v-text="'Boring Normal Text'"></p>
+        <p v-html="'<strong> Cool Strong Text </strong>'"></p>
+      </div>
         <hr>
-        <div class="row" v-if="isSubmitted">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4>Your Data</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Mail: {{ userData.email }}</p>
-                        <p>Password: {{ userData.password }}</p>
-                        <p>Age: {{ userData.age }} </p>
-                        <p style="white-space: pre">Message: {{ message }}</p>
-                        <p><strong>Send Mail?</strong></p>
-                        <ul>
-                            <li v-for="item in sendMail" v-bind:key="item">{{ item }}</li>
-                        </ul>
-                        <p>Gender: {{ gender }}</p>
-                        <p>Priority: {{ selectedPriority }}</p>
-                        <p>Switched: {{ dataSwitch }}</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6  col-md=offset-3">
+        <h1>Custom Directives</h1>
+        <p v-highlight:background.delayed="'red'">Color Me!</p>
+        <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor:'green', delay:500}">Color Me Too!</p>
+    </div>
     </div>
 </template>
 
@@ -150,30 +23,45 @@
 <!-- S C R I P T -->
 
 <script>
-    import CalmedsChilds from './Switcheroo.vue'
     export default {
       data:() => ({
-        userData: {
-            email:'',
-            password:'',
-            age:'0',
-        },
-        message:'*Insert complaint here*',
-        sendMail:[],
-        gender:'Male',
-        selectedPriority:'High',
-        priorities:['High', 'Medium', 'Low'],
-        dataSwitch: true,
-        isSubmitted:false
       }),
-        methods:{
-            submitted() {
-                this.isSubmitted= true;
+      directives: {
+        'local-highlight': {
+          bind(el,binding,vnode){
+            var delay = 0;
+            if (binding.modifiers['delayed']) {
+                delay = 3000;
             }
-        },
-      components: {
-          appSwitch: CalmedsChilds
-      }
+            if (binding.modifiers['blink']) {
+                let mainColor = binding.value.mainColor;
+                let secondColor = binding.value.secondColor;
+                let currentColor = mainColor;
+                setTimeout(() => {
+                  setInterval(() => {
+                    currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
+                    if (binding.arg == ['background']) {
+                      el.style.backgroundColor = currentColor; 
+                    } else {
+                        el.style.color = currentColor;
+                    }
+                    }, binding.value.delay);
+                  
+              }, delay);
+            } else {
+              setTimeout(() => {
+                if (binding.arg == ['background']) {
+                  el.style.backgroundColor = binding.value.mainColor; 
+                } else {
+                  el.style.color = binding.value.mainColor;
+                }
+              }, delay);
+            } 
+            
+          }
+        }
+      },
+        
     }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,16 +1,31 @@
 import Vue from 'vue'
 import CalmsDownsChilds from './App.vue'
-import CalmedsChilds from './Switcheroo.vue'
+
+Vue.directive('highlight' , {
+  bind(el,binding,vnode) {
+      //el.style.backgroundColor = 'green';
+      //el.style.backgroundColor = binding.value; 
+      var delay = 0;
+      if (binding.modifiers['delayed']) {
+        delay = 3000;
+      }
+      setTimeout(() => {
+        if (binding.arg == 'background') {
+          el.style.backgroundColor = binding.value;
+        } else {
+          el.style.color = binding.value;
+        }
+        },delay);
+    }
+      
+});
 
 new Vue({
   el: '#childscalmsdowns',
   render: h => h(CalmsDownsChilds)
-}),
-
-new Vue({
-  el: '#childscalmedsdowns',
-  render: h => h(CalmedsChilds)
 })
+
+
 
 
 
